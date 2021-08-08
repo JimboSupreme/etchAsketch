ece9ba88fbefb3e5e657370325e19a5e7ce4bbd7
Logan Pepple <l.pepple.96@gmail.com>
2021-08-08 00:51:24 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const sketchBox = document.getElementById('sketch-box');
+const gridSize = document.querySelector('input[type=text]');
+const resetButton = document.querySelector('input[type=button]');
+
+resetButton.addEventListener('click', buildGrid);
+
+function removeAllChildNodes(parent) {
+    console.log('erasing children');
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function colorNode(node) {
+    console.log(this);
+    this.classList.add('colored');
+}
+
+function buildGrid() {
+    console.log('building grid');
+    let x = parseInt(gridSize.value);
+    removeAllChildNodes(sketchBox);
+    if (Number.isInteger(x) && x>0 && x<101) {
+        for (let i = 1; i <= x; i++) { //generate rows
+            let row = document.createElement('div');
+            for (let i = 1; i <= x; i++) { //generate cells in each row
+                let cell = document.createElement('div');
+                cell.style.height = (75/x)-(3/x)+'vmin';
+                cell.style.width = (75/x)-(3/x)+'vmin';
+                row.appendChild(cell);
+            }
+            row.style.height = (75/x)-(3/x)+'vmin';
+            if (i == 1) {  //checks for first and last rows for styling purposes
+                row.classList.add('first-row');
+            } else if (i == x) {
+                row.classList.add('last-row');
+            }
+            sketchBox.appendChild(row);
+        }
+        const cells = document.querySelectorAll('#sketch-box>div>div');
+        cells.forEach(cell => cell.addEventListener('mouseover', colorNode));
+    } else {
+        console.log('adding warning');
+        let warning = document.createElement('p');
+        warning.innerText = 'enter grid size from 1-100';
+        sketchBox.appendChild(warning);
+    }
+}
+
